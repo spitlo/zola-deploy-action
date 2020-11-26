409a8b12cf8e4a9ec60f589416d36e7c1699f8ce
spitlo <jakob@aino.se>
2020-11-27 00:32:12 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,6 @@ if [[ -n "$TOKEN" ]]; then
   GITHUB_TOKEN=$TOKEN
 fi
 
-if [[ -z "$PAGES_BRANCH" ]]; then
-  PAGES_BRANCH="master"
-fi
-
 if [[ -z "$GITHUB_TOKEN" ]]; then
   echo "Set the GITHUB_TOKEN env variable."
   exit 1
@@ -29,7 +25,7 @@ main() {
   version=$(zola --version)
 
   remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-  remote_branch=$PAGES_BRANCH
+  remote_branch=master
 
   echo "Using $version"
 
