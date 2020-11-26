1d1c396141bd16945e868f3c11ea4388c9b04a08
spitlo <jakob@aino.se>
2020-11-27 00:09:26 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,53 +1,52 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -eu
 set -o pipefail
 
 if [[ -n "$TOKEN" ]]; then
-    GITHUB_TOKEN=$TOKEN
+  GITHUB_TOKEN=$TOKEN
 fi
 
 if [[ -z "$PAGES_BRANCH" ]]; then
-    PAGES_BRANCH="gh-pages"
+  PAGES_BRANCH="master"
 fi
 
 if [[ -z "$GITHUB_TOKEN" ]]; then
-    echo "Set the GITHUB_TOKEN env variable."
-    exit 1
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
 fi
 
 if [[ -z "$GITHUB_REPOSITORY" ]]; then
-    echo "Set the GITHUB_REPOSITORY env variable."
-    exit 1
+  echo "Set the GITHUB_REPOSITORY env variable."
+  exit 1
 fi
 
 main() {
-    echo "Starting deploy..."
+  echo "Starting deploy..."
 
-    echo "Fetching themes"
-    git config --global url."https://".insteadOf git://
-    git config --global url."https://github.com/".insteadOf git@github.com:
-    git submodule update --init --recursive
+  git config --global url."https://".insteadOf git://
+  git config --global url."https://github.com/".insteadOf git@github.com:
 
-    version=$(zola --version)
-    remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-    remote_branch=$PAGES_BRANCH
+  version=$(zola --version)
 
-    echo "Using $version"
+  remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  remote_branch=$PAGES_BRANCH
 
-    zola build
+  echo "Using $version"
 
-    echo "Pushing artifacts to ${GITHUB_REPOSITORY}:$remote_branch"
+  zola check
+  zola build --output-dir ./docs
 
-    cd public
-    git init
-    git config user.name "GitHub Actions"
-    git config user.email "github-actions-bot@users.noreply.github.com"
-    git add .
+  sed -i '' 's_https://spitlo.com__g' docs/search_index.en.js
 
-    git commit -m "Deploy ${GITHUB_REPOSITORY} to ${GITHUB_REPOSITORY}:$remote_branch"
-    git push --force "${remote_repo}" master:${remote_branch}
+  echo "Pushing artifacts to ${GITHUB_REPOSITORY}:$remote_branch"
 
-    echo "Deploy complete"
+  git add ./docs
+  git commit -m "Deploy site $(date '+%Y-%m-%d %H:%M')"
+  git push
+
+  git push --force "${remote_repo}" master:${remote_branch}
+
+  echo "Deploy complete"
 }
 
 main "$@"
